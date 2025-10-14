11fd6c68ddefd6f5478fbac295b85433c0ef5bae
dismoralic <dismoralic@gmail.com>
2025-10-14 22:10:57 +0500

    Update faust.py

--- modules/faust.py
+++ modules/faust.py
@@ -25,7 +25,7 @@ def register(client):
         ping_ms = (time.perf_counter() - start) * 1000
 
         text = (
-            "𝕱𝖆𝖚𝖘𝖙-𝕿𝖔𝖔𝖑\n"
+            "𝕱𝖆𝖚𝖘𝖙-𝕿𝖔𝖔𝖑 2\n"
             "FTG + Native modules userbot client.\n"
             "Version: 1.0.0\n\n"
             f"Ping: {ping_ms:.2f} ms\n\n"
@@ -36,3 +36,4 @@ def register(client):
 
         await sent.edit(text)
         await event.delete()
+
